f09bee4dddc444c3cee47579e479fa63fc31ef06
Charlie <wl00606352@gmail.com>
2021-07-27 02:04:37 +0800

    add simple layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { ProgressBar, Container, Form } from "react-bootstrap"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="w-100">
+        <div className="my-2 w-75 d-flex align-items-center mx-auto">
+          <label className="w-25">{`12\xB0C`}</label>
+          <ProgressBar className="w-75" now={50} />
+        </div>
+        <div className="my-2 w-75 d-flex align-items-center mx-auto">
+          <label className="w-25">{`12\xB0F`}</label>
+          <ProgressBar className="w-75" now={50} />
+        </div>
+        <div className="my-2 w-75 d-flex align-items-center mx-auto">
+          <label className="w-25">{`12 K`}</label>
+          <ProgressBar className="w-75" now={50} />
+        </div>
+        <Form className="mt-5">
+          <Form.Group className="w-100 d-flex justify-content-center">
+            <Form.Control
+              type="number"
+              className="w-25 mx-1 bg-transparent border-0 border-bottom rounded-0  text-white reset"
+              dir="rtl"
+            ></Form.Control>
+            <Form.Select className="w-25 bg-transparent border-0 border-bottom rounded-0 text-light reset">
+              <option value="tempC">{"\xB0C"}</option>
+              <option value="tempF">{"\xB0F"}</option>
+              <option value="tempK">{"K"}</option>
+            </Form.Select>
+          </Form.Group>
+        </Form>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
