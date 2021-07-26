3c09ef5f20f9ab3b13b47333d7b5cda28b9e88f9
Charlie <wl00606352@gmail.com>
2021-07-27 02:34:31 +0800

    add font and re-layout

--- src/App.js
+++ src/App.js
@@ -5,26 +5,35 @@ function App() {
   return (
     <div className="App">
       <Container className="w-100">
-        <div className="my-2 w-75 d-flex align-items-center mx-auto">
-          <label className="w-25">{`12\xB0C`}</label>
-          <ProgressBar className="w-75" now={50} />
+        <div className="w-75 d-flex mx-auto mb-5 rounded-3 border border-light bg-light text-dark">
+          <h1 className="w-100 text-center my-1">Temperature Converter</h1>
         </div>
-        <div className="my-2 w-75 d-flex align-items-center mx-auto">
-          <label className="w-25">{`12\xB0F`}</label>
-          <ProgressBar className="w-75" now={50} />
+
+        <div className="text-start">
+          <div className="my-2 w-75 d-flex align-items-center mx-auto">
+            <label className="w-25">{`12\xB0C`}</label>
+            <ProgressBar className="w-75" now={50} />
+          </div>
+          <div className="my-2 w-75 d-flex align-items-center mx-auto">
+            <label className="w-25">{`12\xB0F`}</label>
+            <ProgressBar className="w-75" now={50} />
+          </div>
+          <div className="my-2 w-75 d-flex align-items-center mx-auto">
+            <label className="w-25">{`120 K`}</label>
+            <ProgressBar className="w-75" now={50} />
+          </div>
         </div>
-        <div className="my-2 w-75 d-flex align-items-center mx-auto">
-          <label className="w-25">{`12 K`}</label>
-          <ProgressBar className="w-75" now={50} />
-        </div>
-        <Form className="mt-5">
-          <Form.Group className="w-100 d-flex justify-content-center">
+
+        <Form className="mt-5 d-flex justify-content-center">
+          <Form.Group className="w-50 d-flex justify-content-center rounded-3 border border-light bg-light text-dark py-1 mx-1">
             <Form.Control
               type="number"
-              className="w-25 mx-1 bg-transparent border-0 border-bottom rounded-0  text-white reset"
+              className=" mx-1 bg-transparent border-0 rounded-0  reset"
               dir="rtl"
             ></Form.Control>
-            <Form.Select className="w-25 bg-transparent border-0 border-bottom rounded-0 text-light reset">
+          </Form.Group>
+          <Form.Group className="w-25 d-flex justify-content-center rounded-3 border border-light bg-light text-dark py-1 mx-1">
+            <Form.Select className=" bg-transparent border-0  rounded-0  reset">
               <option value="tempC">{"\xB0C"}</option>
               <option value="tempF">{"\xB0F"}</option>
               <option value="tempK">{"K"}</option>
