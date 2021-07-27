a0b048e51706ce3c8144f069a709ea95eab6957e
Charlie <wl00606352@gmail.com>
2021-07-27 16:20:18 +0800

    re-factor

--- src/components/TempInputs.js
+++ src/components/TempInputs.js
@@ -7,41 +7,35 @@ const TempInputs = () => {
   const [tempK, setTempK] = useState(273)
 
   const getTempC = (event) => {
-    setTempC(event.target.value)
-    let F = 32 + (9 / 5) * +event.target.value
-    let K = 273 + +event.target.value
-    setTempF(F)
-    setTempK(K)
+    setTempC(+event.target.value)
+    setTempF((32 + (9 / 5) * +event.target.value).toFixed(2))
+    setTempK((273 + +event.target.value).toFixed(2))
   }
 
   const getTempF = (event) => {
-    setTempF(event.target.value)
-    let C = -32 + (5 / 9) * +event.target.value
-    let K = C + 273
-    setTempC(C)
-    setTempK(K)
+    setTempF(+event.target.value)
+    setTempC((-32 + (5 / 9) * +event.target.value).toFixed(2))
+    setTempK((((+event.target.value + 459.67) * 5) / 9).toFixed(2))
   }
 
   const getTempK = (event) => {
-    setTempF(event.target.value)
-    let C = -273 + +event.target.value
-    let F = 32 + 9 / 5(-273 + +event.target.value)
-    setTempC(C)
-    setTempF(F)
+    setTempK(+event.target.value)
+    setTempC((-273 + +event.target.value).toFixed(2))
+    setTempF((32 + (9 / 5) * (-273 + +event.target.value)).toFixed(2))
   }
 
   return (
     <Row>
-      <Col id="tempC" xs={12} sm={4}>
-        <Form.Control type="number" value={tempC} onChange={getTempC} />
+      <Col id="tempC" xs={12} xl={4}>
+        <Form.Control as="textarea" value={tempC} onChange={getTempC} />
         <div className="tempNum">{"\xB0"}C</div>
       </Col>
-      <Col id="tempF" xs={12} sm={4}>
-        <Form.Control type="number" value={tempF} onChange={getTempF} />
+      <Col id="tempF" xs={12} xl={4}>
+        <Form.Control as="textarea" value={tempF} onChange={getTempF} />
         <div className="tempNum">{"\xB0"}F</div>
       </Col>
-      <Col id="tempK" xs={12} sm={4}>
-        <Form.Control type="number" value={tempK} onChange={getTempK} />
+      <Col id="tempK" xs={12} xl={4}>
+        <Form.Control as="textarea" value={tempK} onChange={getTempK} />
         <div className="tempNum">K.</div>
       </Col>
     </Row>
